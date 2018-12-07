0e3ec3f385ec68d7a4f406599778986df7187fe7
rubysih <rr910052@gmail.com>
2018-12-07 15:54:44 +0800

    build

---
+++ build/precache-manifest.369f796e4712f4b441b08ac2bbb59e29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "89d803846aec49b2a183",
+    "url": "./static/js/main.89d80384.chunk.js"
+  },
+  {
+    "revision": "e9132f8ab16f178407b9",
+    "url": "./static/js/1.e9132f8a.chunk.js"
+  },
+  {
+    "revision": "0e1834e7f4527947512fc2bac27b25ea",
+    "url": "./index.html"
+  }
+];
